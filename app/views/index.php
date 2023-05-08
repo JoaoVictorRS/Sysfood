b81157f3164d571f01f2edbbf8df3ca00f6b56f4
Kelvin Alves Moura <kelvinestra@gmail.com>
2023-05-07 23:07:54 -0300

    mudança de imagen, apenas um teste

--- app/views/index.php
+++ app/views/index.php
@@ -207,7 +207,7 @@ session_start();
                         </div>
                         <div class="col-lg-6">
                             <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                                <img src="../assets/images/slider-dec.png" alt="">
+                                <img src="../assets/images/restaurante.png" alt="">
                             </div>
                         </div>
                     </div>
