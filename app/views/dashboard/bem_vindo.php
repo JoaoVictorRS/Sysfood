c7518bdee9be1b0e264ef8bbcd72e17eb9ea6430
João Victor <joaososteam@gmail.com>
2023-05-13 23:22:23 -0300

    Login fase2 em andamento

--- app/views/dashboard/bem_vindo.php
+++ app/views/dashboard/bem_vindo.php
@@ -1,6 +1,11 @@
 <?php require_once '../../views/layouts/user/header.php'; ?>
 <?php require_once '../../views/layouts/user/left_menu.php'; ?>
 <div class="container">
+    <?php
+        session_start();
+        var_dump($_SESSION);
+        echo session_id();
+    ?>
 
 </div>
 <?php require_once '../../views/layouts/user/footer.php'; ?>
--- app/views/site/login.php
+++ app/views/site/login.php
@@ -1,16 +1,15 @@
 <?php
     require_once "../../models/database/conexao.php";
 
-    session_start();
-
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $error = [];
         $email = isset($_POST['email']) ? $_POST['email'] : '';
-        $senha = isset($_POST['senha']) ? md5($_POST['senha']) : '';
+        //função md5 temporariamente removida
+        $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
         $dbh = Conexao::getInstance();
-    
-
+        
+        //Query de verificação se é uma empresa
         $query = "SELECT * FROM `sysfood`.`empresas` WHERE email = :email AND senha = :senha";
         $stmt = $dbh->prepare($query);
         $stmt->bindParam(':email', $email);
@@ -18,16 +17,40 @@
 
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-       
-        if($row) {
+
+        //Query de verificação se é um funcionario
+        $query_F = "SELECT * FROM `sysfood`.`funcionarios` INNER JOIN `sysfood`.`usuarios` ON `funcionarios`.`usuario_id` = `usuarios`.`id` WHERE email = :email AND senha = :senha";
+        $stmt_F = $dbh->prepare($query_F);
+        $stmt_F->bindParam(':email', $email);
+        $stmt_F->bindParam(':senha', $senha);
+
+        $stmt_F->execute();
+        $row_F = $stmt_F->fetch(PDO::FETCH_ASSOC);
+
+        if($row){
             $_SESSION['usuario'] = [
                 'email' => $row['email'],
             ];
-            header('location: ../sessoes/');
-        } else {
+            header('location: ../dashboard/bem_vindo.php');
+        }else if ($row_F) {
+            //Query que pega o cargo do otario
+            $query_cargo = "SELECT cargo FROM `sysfood`.`funcionarios` WHERE id = :id";
+            $stmt_cargo = $dbh->prepare($query_cargo);
+            $stmt_cargo->bindParam(':id', $row_F['id']);
+
+            $stmt_cargo->execute();
+            $row_cargo = $stmt_cargo->fetch(PDO::FETCH_ASSOC);
+
+            $_SESSION['usuario'] = [
+                'email' => $row_F['email'],
+                'cargo' => $row_cargo['cargo']
+            ];
+            header('location: ../dashboard/bem_vindo.php');
+        }else{
             session_destroy();
             header('location: ../index.php?error=Usuário ou senha inválidos.');
         }
+
     }
 ?>
 <!--POP LOGIN-->
