d36e8cde0f6c2f9ae11d6df6a6f8994e78e8baff
João Victor <joaososteam@gmail.com>
2023-05-21 08:59:27 -0300

    Separando footer header e body, tudo funcionando

---
+++ app/views/site/footer.php
@@ -0,0 +1,24 @@
+<footer id="newsletter">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="copyright-text">
+                        <p>Copyright © 2023 Sysfood. All Rights Reserved.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Scripts -->
+    <script src="sweetalert2.all.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/owl-carousel.js"></script>
+    <script src="../assets/js/animation.js"></script>
+    <script src="../assets/js/imagesloaded.js"></script>
+    <script src="../assets/js/popup.js"></script>
+    <script src="../assets/js/custom.js"></script>
+</body>
+
+</html>
